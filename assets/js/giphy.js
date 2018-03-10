e086bb6343a2d8a351d383345c3238d1a92d5151
Allan Jones <iamallanjones@gmail.com>
2018-03-10 16:31:56 -0600

    Fixed Ajax Callback and api display on page

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -1,64 +1,98 @@
  
      // Initial array of emotions
-    var emotionArr = ["Happy", "Calm", "Excited", "Sad"];
+     var emotionArr = ["Happy", "Calm", "Excited", "Sad"];
 
-    // Function for dumping the JSON content for each button into the div
-    function displayEmotions() {
-
-      // YOUR CODE GOES HERE!!! HINT: You will need to create a new div to hold the JSON.
-      var emotion = $(this).attr("data-name");
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=f" + emotion + "&api_key=dc6zaTOxFJmzC";
-
-      $.ajax({
-         url: queryURL,
-         method: "GET"
-       }).then(function (response){
-        console.log(response);
-       });
-
-    }
-
-    // Function for displaying movie data
-    function renderButtons() {
-
-      // Deleting the buttons prior to adding new movies
-      // (this is necessary otherwise you will have repeat buttons)
-      $("#emoButtons").empty();
-
-      // Looping through the array of emotion
-      for (var i = 0; i < emotionArr.length; i++) {
-
-        // Then dynamicaly generating buttons for each movie in the array
-        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-        var a = $("<button>");
-        // Adding a class of movie to our button
-        a.addClass("movie");
-        // Adding a data-attribute
-        a.attr("data-name", emotionArr[i]);
-        // Providing the initial button text
-        a.text(emotionArr[i]);
-        // Adding the button to the buttons-view div
-        $("#emoButtons").append(a);
-      }
-    }
-
-    // This function handles events where one button is clicked
-    $("#addEmotion").on("click", function(event) {
-      event.preventDefault();
-
-      // This line grabs the input from the textbox
-      var emotionInput = $("#emotionInput").val().trim();
-
-      // The emotion input from the textbox is then added to our array
-      emotionArr.push(emotionInput);
-
-      // Calling renderButtons which handles the processing of our movie array
-      renderButtons();
-
-    });
-
-    // Generic function for displaying the movieInfo
-    $(document).on("click", ".movie", displayEmotions);
-
-    // Calling the renderButtons function to display the intial buttons
-    renderButtons();
+     // Function for dumping the JSON content for each button into the div
+     function displayEmotions() {
+ 
+       // Query Giphy
+       var emotion = $(this).attr("data-name");
+       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+         emotion + "&api_key=dc6zaTOxFJmzC&limit=10";
+      
+       // AJAX Callback
+       $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function(response) {
+            // Storing an array of results in the results variable
+            var results = response.data;
+  
+            // Looping over every result item
+            for (var i = 0; i < results.length; i++) {
+  
+              // Only taking action if the photo has an appropriate rating
+              if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                // Creating a div with the class "item"
+                var gifDiv = $("<div class='item'>");
+  
+                // Storing the result item's rating
+                var rating = results[i].rating;
+  
+                // Creating a paragraph tag with the result item's rating
+                var p = $("<p>").text("Rating: " + rating);
+  
+                // Creating an image tag
+                var gifImage = $("<img>");
+  
+                // Giving the image tag an src attribute of a property pulled off the
+                // result item
+                gifImage.attr("src", results[i].images.fixed_height.url);
+  
+                // Appending the paragraph and personImage we created to the "gifDiv" div we created
+                gifDiv.append(p);
+                gifDiv.append(gifImage);
+  
+                // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+                $("#gifs-appear-here").prepend(gifDiv);
+              }
+            }
+          });
+ 
+     }
+ 
+     // Function for displaying emotion data
+     function renderButtons() {
+ 
+       // Deleting the buttons prior to adding new emotion
+       // (this is necessary otherwise you will have repeat buttons)
+       $("#emoButtons").empty();
+ 
+       // Looping through the array of emotion
+       for (var i = 0; i < emotionArr.length; i++) {
+ 
+         // Then dynamicaly generating buttons for each emotion in the array
+         // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+         var btn = $("<button>");
+         // Adding a class of movie to our button
+         btn.addClass("btn btn-default feeling");
+         // Adding a data-attribute
+         btn.attr("data-name", emotionArr[i]);
+         // Providing the initial button text
+         btn.text(emotionArr[i]);
+         // Adding the button to the buttons-view div
+         $("#emoButtons").append(btn);
+       }
+     }
+ 
+     // This function handles events where one button is clicked
+     $("#addEmotion").on("click", function(event) {
+       event.preventDefault();
+ 
+       // This line grabs the input from the textbox
+       var emotionInput = $("#emotionInput").val().trim();
+ 
+       // The emotion input from the textbox is then added to our array
+       emotionArr.push(emotionInput);
+       console.log(emotionArr);
+ 
+       // Calling renderButtons which handles the processing of our movie array
+       renderButtons();
+ 
+     });
+ 
+     // Generic function for displaying the movieInfo
+     $(document).on("click", ".feeling", displayEmotions);
+ 
+     // Calling the renderButtons function to display the intial buttons
+     renderButtons();
